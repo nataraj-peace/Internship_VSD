154bda6fe0aee618088895c56f6837840d51568a
nataraj-peace <natarajan.ece.snsce@gmail.com>
2026-01-19 12:41:28 +0530

    Update main.c

--- Task2/submission/main.c
+++ Task2/submission/main.c
@@ -11,6 +11,14 @@ int main(void)
 
     USART1_Init();   // from previous UART task
 
+    delay_ms(100);
+
+    USART1_SendString("\r\n=================================\r\n");
+    USART1_SendString("Board Name      : VSDSquadron Mini\r\n");
+    USART1_SendString("Firmware Version: v1.0.0\r\n");
+    USART1_SendString("Interface       : USART1 (PD5) -> ttyACM0\r\n");
+    USART1_SendString("=================================\r\n");
+
     USART1_SendString("\r\nTask-2 GPIO + UART Demo\r\n");
     USART1_SendString("GPIO : PD4 (GPIOD, GPIO_Pin_4)\r\n");
 
